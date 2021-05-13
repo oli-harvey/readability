edfa45094bb9e4386d585c924b4d345151beff8b
Oli Harvey <oliharvey@hotmail.com>
2021-05-13 08:13:23 +0100

    it begins

---
+++ functions/get_kaggle_data.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import zipfile
+
+def get_kaggle_data(
+        competition: str,
+        # competition_name: str, 
+        dir: str = "competition_data"
+        ) -> [str]:
+    """
+        Uses Kaggle API to download zip file of all kaggle competition data.
+        Saves it in data directory, default competition_data
+        Extracts files
+        Deletes the zip file
+        Returns list of files downloaded
+    """
+
+    dl_zip = [
+        f"mkdir {dir}",
+        f"kaggle competitions download -c {competition} -p {dir}",
+    ]
+
+    [
+       subprocess.Popen(step, shell=True).wait()
+       for step in dl_zip
+    ]
+    zip_path = f"{dir}/{competition}.zip"
+
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(f"{dir}/")
+    
+    rm_zip = f"rm {zip_path}"
+    subprocess.Popen(rm_zip, shell=True).wait()
+
+    print(f'files downloaded for {competition} competition')
+    # print('   ' + os.listdir(dir))
+    files = os.listdir(dir)
+    [
+        print('   ' + file)
+        for file in files   
+    ]
+    print(f'located in /{dir}/')
+    return files
---
+++ functions/import_to_df.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def import_to_df(
+        list: str,
+        path: str = "competition_data"
+         ) -> dict:
+    """
+        Imports all the files in the list given
+        Returns dictionary with file name minus extension as 
+    """
+
+    df_dict = {}
+    for file in list:
+        if 'csv' not in file:
+            continue
+        df = pd.read_csv("/".join([path, file]))
+        # remove extension
+        name = file.split('.')[0]
+        df_dict[name] = df
+    
+    return df_dict
+[
+
+]

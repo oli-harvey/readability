3fd8e79751bf8e4668301e86d219a3323f7cd92e
Oli Harvey <oliharvey@hotmail.com>
2021-05-25 18:43:16 +0100

    added avg word length

---
+++ functions/avg_word_length.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def avg_word_length(
+        df: pd.DataFrame,
+        col: str
+        ) -> pd.DataFrame:
+    out_df = df.copy()
+    new_col_name = col + '_avg_word_count'
+    out_df[new_col_name] = out_df[col].apply(_avg_word_len)
+    return out_df
+
+def _avg_word_len(x):
+    words = x.split()
+    word_lengths = [len(word) for word in words]
+    avg_word_length = sum(word_lengths)/len(words)
+    return avg_word_length
